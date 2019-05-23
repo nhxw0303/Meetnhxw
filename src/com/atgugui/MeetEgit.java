e9c99c375cebc840c6f1fc34f4bb370e28cb8e94
niaodan <nhxw0303@163.com>
2019-05-23 17:22:53 +0800

    clone 更新

--- src/com/atgugui/MeetEgit.java
+++ src/com/atgugui/MeetEgit.java
@@ -1,8 +1,10 @@
-package com.atguigu;
+package com.atgugui;
+
 
 public class MeetEgit {
 
 	public static void main(String[] args) {
 		System.out.println("nice to meet you");
+		System.out.println("nhxw nice to meet you");
 	}
 }
