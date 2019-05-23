c3858baacb5a2f76a4ed01e3c3c4aa26a10a84c4
niaodan <nhxw0303@163.com>
2019-05-23 17:26:13 +0800

    更新-clone

--- src/com/atgugui/MeetEgit.java
+++ src/com/atgugui/MeetEgit.java
@@ -6,5 +6,6 @@ public class MeetEgit {
 	public static void main(String[] args) {
 		System.out.println("nice to meet you");
 		System.out.println("nhxw nice to meet you");
+		System.out.println("nhxw nice to meet you,clone");
 	}
 }
